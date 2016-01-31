0315917dab549220614ce135ebcc593e2d1a5c3c
Martin Podlubny <martin.podlubny@gmail.com>
2016-01-31 09:09:10 +0000

    I extract items away

--- js/game.js
+++ js/game.js
@@ -1,114 +1,6 @@
 (function () {
     var game = new Phaser.Game(1024, 768, Phaser.AUTO, '');
 
-    var itemCollection = [{
-        name: 'google',
-        image: 'assets/icon_google.png'
-    }, {
-        name: 'mlh',
-        image: 'assets/icon_mlh.png'
-    }, {
-        name: 'thales',
-        image: 'assets/icon_thales.png'
-    }, {
-        name: 'uoc',
-        image: 'assets/uoc_logo.png'
-    }, {
-        name: 'redbull',
-        image: 'assets/icon_redbull.png'
-    }, {
-        name: 'beer',
-        image: 'assets/icon_beer.png'
-    }, {
-        name: 'gsa',
-        image: 'assets/icon_gsa.png'
-    }, {
-        name: 'cantab',
-        image: 'assets/icon_cantab.png'
-    }];
-
-    //10%
-    var topCompanies = [
-        {
-            name: 'google',
-            image: 'assets/icon_google.png',
-            effect: {
-                dXP: 1,
-                dSpeed: 0,
-                dScale: 1.0
-            }
-        },
-        {
-            name: 'thales',
-            image: 'assets/icon_thales.png',
-            effect: {
-                dXP: 1,
-                dSpeed: 0,
-                dScale: 1.0
-            }
-        }
-    ];
-    //25%
-    var regularCompanies = [
-        {
-            name: 'gsa',
-            image: 'assets/icon_gsa.png',
-            effect: {
-                dXP: 1,
-                dSpeed: 0,
-                dScale: 1.0
-            }
-        },
-        {
-            name: 'cantab',
-            image: 'assets/icon_cantab.png',
-            effect: {
-                dXP: 1,
-                dSpeed: 0,
-                dScale: 1.0
-            }
-        }
-    ];
-    //15%
-    var studies = {
-        name: 'uoc', image: 'assets/uoc_logo.png',
-        effect: {
-            dXP: 5,
-            dSpeed: 100,
-            dScale: 1.2
-        }
-    };
-    //20%
-    var recreation = [
-        {
-            name: 'beer',
-            image: 'assets/icon_beer.png',
-            effect: {
-                dXP: -1,
-                dSpeed: -100,
-                dScale: 1.5
-            }
-        },
-        {
-            name: 'redbull',
-            image: 'assets/icon_redbull.png',
-            effect: {
-                dXP: 0,
-                dSpeed: 500,
-                dScale: 0.8
-            }
-        }
-    ];
-    //30%
-    var hackathon = {
-        name: 'mlh', image: 'assets/icon_mlh.png',
-        effect: {
-            dXP: 3,
-            dSpeed: 0,
-            dScale: 1.0
-        }
-    };
-
     var p1, p2;
     var exp1, exp2;
 
@@ -404,8 +296,6 @@
         },
         create: function () {
             game.add.sprite(0, 0, 'start');
-
-
         },
         start: function () {
 
---
+++ js/item-definitions.js
@@ -0,0 +1,107 @@
+var itemCollection = [{
+    name: 'google',
+    image: 'assets/icon_google.png'
+}, {
+    name: 'mlh',
+    image: 'assets/icon_mlh.png'
+}, {
+    name: 'thales',
+    image: 'assets/icon_thales.png'
+}, {
+    name: 'uoc',
+    image: 'assets/uoc_logo.png'
+}, {
+    name: 'redbull',
+    image: 'assets/icon_redbull.png'
+}, {
+    name: 'beer',
+    image: 'assets/icon_beer.png'
+}, {
+    name: 'gsa',
+    image: 'assets/icon_gsa.png'
+}, {
+    name: 'cantab',
+    image: 'assets/icon_cantab.png'
+}];
+
+//10%
+var topCompanies = [
+    {
+        name: 'google',
+        image: 'assets/icon_google.png',
+        effect: {
+            dXP: 1,
+            dSpeed: 0,
+            dScale: 1.0
+        }
+    },
+    {
+        name: 'thales',
+        image: 'assets/icon_thales.png',
+        effect: {
+            dXP: 1,
+            dSpeed: 0,
+            dScale: 1.0
+        }
+    }
+];
+//25%
+var regularCompanies = [
+    {
+        name: 'gsa',
+        image: 'assets/icon_gsa.png',
+        effect: {
+            dXP: 1,
+            dSpeed: 0,
+            dScale: 1.0
+        }
+    },
+    {
+        name: 'cantab',
+        image: 'assets/icon_cantab.png',
+        effect: {
+            dXP: 1,
+            dSpeed: 0,
+            dScale: 1.0
+        }
+    }
+];
+//15%
+var studies = {
+    name: 'uoc', image: 'assets/uoc_logo.png',
+    effect: {
+        dXP: 5,
+        dSpeed: 100,
+        dScale: 1.2
+    }
+};
+//20%
+var recreation = [
+    {
+        name: 'beer',
+        image: 'assets/icon_beer.png',
+        effect: {
+            dXP: -1,
+            dSpeed: -100,
+            dScale: 1.5
+        }
+    },
+    {
+        name: 'redbull',
+        image: 'assets/icon_redbull.png',
+        effect: {
+            dXP: 0,
+            dSpeed: 500,
+            dScale: 0.8
+        }
+    }
+];
+//30%
+var hackathon = {
+    name: 'mlh', image: 'assets/icon_mlh.png',
+    effect: {
+        dXP: 3,
+        dSpeed: 0,
+        dScale: 1.0
+    }
+};
